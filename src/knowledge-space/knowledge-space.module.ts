32407b3ee9a0a1b63bb977e8d59e2b5918788cc5
nedeljkovignjevic <vignjevic.nedeljko1999@gmail.com>
2024-03-12 20:32:25 +0100

    bugfix: KSTRelationService.

--- src/knowledge-space/knowledge-space.module.ts
+++ src/knowledge-space/knowledge-space.module.ts
@@ -5,9 +5,10 @@ import { KnowledgeSpace } from './knowledge-space.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { KSTNode } from 'src/kst-node/kst-node.entity';
 import { KstNodeModule } from 'src/kst-node/kst-node.module';
+import { KstRelationModule } from 'src/kst-relation/kst-relation.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([KnowledgeSpace]), KstNodeModule],
+  imports: [TypeOrmModule.forFeature([KnowledgeSpace]), KstNodeModule, KstRelationModule],
   providers: [KnowledgeSpaceService],
   exports: [KnowledgeSpaceService],
   controllers: [KnowledgeSpaceController]
