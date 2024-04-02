483aba3558693d005842bfc6f7e1c7e52eb44877
VukoticMarko <50705538+VukoticMarko@users.noreply.github.com>
2024-04-02 21:08:48 +0200

    Fixed ontology bug, added reading from virtuoso
    Concept2 had 2 x fields and it created bug. Reuploaded fixed ontology with new diagram png

--- src/knowledge-space/virtuoso/virtuoso.service.ts
+++ src/knowledge-space/virtuoso/virtuoso.service.ts
@@ -6,14 +6,77 @@ import { lastValueFrom } from 'rxjs';
 export class VirtuosoService {
   constructor(private httpService: HttpService) {}
 
+  private mapSparqlResultsToGraph(sparqlResults: any): any {
+    const graphs = new Map<string, any>();
+  
+    sparqlResults.forEach((binding: any) => {
+      const graphId = binding.graph.value;
+      let graph = graphs.get(graphId);
+  
+      if (!graph) {
+        graph = {
+          id: binding.graphId.value,
+          graphName: binding.graphName.value,
+          graphDescription: binding.graphDescription.value,
+          concepts: [],
+          links: []
+        };
+        graphs.set(graphId, graph);
+      }
+  
+      // Duplicate checker
+      if (binding.conceptId && !graph.concepts.find(c => c.id === binding.conceptId.value)) {
+        graph.concepts.push({
+          id: binding.conceptId.value,
+          key: binding.key.value,
+          concept: binding.concept.value,
+          x: parseFloat(binding.x.value),
+          y: parseFloat(binding.y.value)
+        });
+      }
+  
+      if (binding.linkId && !graph.links.find(l => l.id === binding.linkId.value)) {
+        graph.links.push({
+          id: binding.linkId.value,
+          source: binding.source.value,
+          target: binding.target.value
+        });
+      }
+    });
+  
+    return Array.from(graphs.values());
+  }
+  
+  
+  
+  // DESCRIBE <http://localhost:8890/graphs#concept1>
   async getAllGraphs(): Promise<any> {
     const query = `
-      PREFIX : <http://localhost:8890/graphs#>
-      SELECT ?graph ?name ?description WHERE {
-        ?graph a :Graph .
-        ?graph :graphName ?name .
-        ?graph :graphDescription ?description .
+    PREFIX : <http://localhost:8890/graphs#>
+
+    SELECT ?graph ?graphId ?graphName ?graphDescription ?cnpt ?concept ?conceptId ?key ?x ?y ?link ?linkId ?source ?target
+    WHERE {
+      ?graph a :Graph ;
+            :graphId ?graphId ;
+            :graphName ?graphName ;
+            :graphDescription ?graphDescription .
+      OPTIONAL {
+        ?graph :hasConcept ?cnpt .
+        ?cnpt
+                :concept ?concept ; 
+                :conceptId ?conceptId ;
+                :key ?key ;
+                :x ?x ;
+                :y ?y .
       }
+      OPTIONAL {
+        ?graph :hasLink ?link .
+        ?link :linkId ?linkId ;
+              :source ?source ;
+              :target ?target .
+      }
+    }
+
     `;
 
     try {
@@ -25,7 +88,7 @@ export class VirtuosoService {
           headers: { Accept: 'application/sparql-results+json' },
         }),
       );
-      return response.data.results.bindings;
+      return this.mapSparqlResultsToGraph(response.data.results.bindings);
     } catch (error) {
       console.error(error);
       throw error;
